f817533a5b45fa833b7f113c8f414acb2b3e0288
Pilar Medina <pilarcamilmedinagarcia@gmail.com>
2026-04-15 11:05:00 -0400

    Tablas HASH, Agregar primeros pasos

---
+++ ContactManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ContactManager
+{
+    Dictionary<string, string> ContactDictionary;
+
+    public ContactManager()
+    {
+        ContactDictionary = new Dictionary<string, string>();
+    }
+    public void AddContact(string name, string phoneNumber)
+    {
+        if (ContactDictionary.ContainsKey(name))
+        {
+            Console.WriteLine("El contacto" + name + "ya existe");
+        }
+        else
+        {
+            ContactDictionary.Add(name, phoneNumber);
+            Console.WriteLine("Contacto agregado");
+        }
+    }
+    public void SearchContact(string name)
+    {
+        if (ContactDictionary.ContainsKey(name))
+        {
+            Console.WriteLine($"Nombre: {name}  , Telefono: {ContactDictionary[name]}" );
+        }
+        else
+        {
+            Console.WriteLine($"El contacto '{name}' no existe en la agenda");
+        }
+    }
+    public void RemoveContact(string name)
+    {
+        if (ContactDictionary.ContainsKey(name))
+        {
+            ContactDictionary.Remove(name);
+            Console.WriteLine($"{name} Ha sido eliminado con exito");
+        }
+        else
+        {
+            Console.WriteLine($"El contacto {name} no se encuntra en la agenda");
+        }
+    }
+    
+}
+    
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+    ContactManager contact = new ContactManager();
+
+    contact.AddContact("Luis", "0993848944");
+    contact.AddContact("Yatna", "0993848944");
+    contact.AddContact("Cali", "0993848944");
+    contact.AddContact("Juana", "0993848944");
+
+    Console.WriteLine("Buscando contacto");
+    contact.SearchContact("Luisa");
+    contact.SearchContact("Luis");
+    contact.SearchContact("Amanda");
+
+    Console.WriteLine("Eliminando contacto");
+    contact.RemoveContact("Luis");
+    contact.RemoveContact("Joana");
+
